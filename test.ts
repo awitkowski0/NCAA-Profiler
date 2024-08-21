2d19601778718918e75b1321779b50fadb63b86a
alexw <alex.j.witkowski@gmail.com>
2024-08-21 14:16:36 -0400

    Added a video of the app running, contains an uncached game so it's a little bit slower

--- test.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Overlay {
-    pixel_paths:       PixelPaths;
-    video_start_frame: number;
-}
-
-export interface PixelPaths {
-    [key: string]: Array<number[]> | undefined;
-}
